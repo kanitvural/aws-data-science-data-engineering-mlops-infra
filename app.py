ff84b6bcc4655993798ec685a85a81829d5303f7
kanitvural <kanitvural@gmail.com>
2025-07-20 15:48:50 +0300

    Add initial data engineering stack

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 import aws_cdk as cdk
 from infra_pipeline.infra_pipeline_stack import InfraPipelineStack
@@ -10,16 +11,7 @@ env = cdk.Environment(
     region=app.node.try_get_context("region") or "eu-central-1"
 )
 
-# Project prefix
-project_name = app.node.try_get_context("project_name") or "data-engineering"
-notification_email = app.node.try_get_context("notification_email")
-
-InfraPipelineStack(
-    app, 
-    "InfraPipelineStack", 
-    env=env,
-    project_name=project_name,
-    notification_email=notification_email
-)
+# Stack'i sadece CDK parametreleriyle oluştur
+InfraPipelineStack(app, "InfraPipelineStack", env=env)
 
 app.synth()
--- infra_pipeline/infra_pipeline_stack.py
+++ infra_pipeline/infra_pipeline_stack.py
@@ -5,17 +5,21 @@ from aws_cdk import (
     aws_codepipeline_actions as codepipeline_actions,
 )
 from constructs import Construct
-from infra_pipeline.data_engineering_stage import DataEngineeringStage
+from .data_engineering_stage import DataEngineeringStage
 
 class InfraPipelineStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs):
         super().__init__(scope, construct_id, **kwargs)
-
+        
+        # Context'ten parametreleri al
+        project_name = self.node.try_get_context("project_name") or "data-engineering"
+        notification_email = self.node.try_get_context("notification_email")
+        
         # GitHub connections information
         github_repo = "kanitvural/aws_data_science_data_engineering_mlops_infra"
         github_branch = "dataengineering"
         connection_arn = self.node.try_get_context("githubConnectionArn")
-
+        
         # Source aşaması
         source = pipelines_.CodePipelineSource.connection(
             repo_string=github_repo,
@@ -32,13 +36,82 @@ class InfraPipelineStack(Stack):
                 "cdk synth",
             ]
         )
-
+        
         # Pipeline'ı oluştur
         pipeline = pipelines_.CodePipeline(
             self,
             "InfraPipeline",
-            synth= synth_step,
+            synth=synth_step,
         )
         
-        data_eng_stage = DataEngineeringStage(self, "DataEngineeringStage")
+        # DataEngineeringStage'i parametrelerle oluştur
+        data_eng_stage = DataEngineeringStage(
+            self, 
+            "DataEngineeringStage",
+            env=self.env,
+            project_name=project_name,
+            notification_email=notification_email
+        )
+        
         pipeline.add_stage(data_eng_stage)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from aws_cdk import (
+#     Stack,
+#     pipelines as pipelines_,
+#     aws_codepipeline as codepipeline,
+#     aws_codepipeline_actions as codepipeline_actions,
+# )
+# from constructs import Construct
+# from infra_pipeline.data_engineering_stage import DataEngineeringStage
+
+# class InfraPipelineStack(Stack):
+#     def __init__(self, scope: Construct, construct_id: str, **kwargs):
+#         super().__init__(scope, construct_id, **kwargs)
+
+#         # GitHub connections information
+#         github_repo = "kanitvural/aws_data_science_data_engineering_mlops_infra"
+#         github_branch = "dataengineering"
+#         connection_arn = self.node.try_get_context("githubConnectionArn")
+
+#         # Source aşaması
+#         source = pipelines_.CodePipelineSource.connection(
+#             repo_string=github_repo,
+#             branch=github_branch,
+#             connection_arn=connection_arn,
+#         )
+        
+#         synth_step = pipelines_.ShellStep(
+#             "Synth",
+#             input=source,
+#             commands=[
+#                 "npm install -g aws-cdk",
+#                 "pip install -r requirements.txt",
+#                 "cdk synth",
+#             ]
+#         )
+
+#         # Pipeline'ı oluştur
+#         pipeline = pipelines_.CodePipeline(
+#             self,
+#             "InfraPipeline",
+#             synth= synth_step,
+#         )
+        
+#         data_eng_stage = DataEngineeringStage(self, "DataEngineeringStage")
+#         pipeline.add_stage(data_eng_stage)
