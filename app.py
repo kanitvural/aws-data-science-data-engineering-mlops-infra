7746484d07cc04aff3441dd98baf20a8962214b6
kanitvural <kanitvural@gmail.com>
2025-07-21 09:53:27 +0300

    UP

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
-
 #!/usr/bin/env python3
 import aws_cdk as cdk
-from data_engineering.cdk_pipeline.cdk_data_engineering_pipeline import CDKPipelineStack
+from data_engineering.cdk_pipeline.cdk_data_engineering_pipeline import CDKDEPipelineStack
 
 app = cdk.App()
 
 # Environment
 env = cdk.Environment(
-    account=app.node.try_get_context("account"),
-    region=app.node.try_get_context("region") or "eu-central-1"
+    account=app.node.try_get_context("account"), region=app.node.try_get_context("region") or "eu-central-1"
 )
 
 # Stack'i sadece CDK parametreleriyle oluştur
-CDKPipelineStack(app, "CDKPipelineStack", env=env)
+CDKDEPipelineStack(app, "CDKPipelineStack", env=env)
 
 app.synth()
 
@@ -26,5 +24,3 @@ app.synth()
 # aws cloudformation delete-stack --stack-name DataEngineeringStage-data-engineering-glue
 # aws cloudformation delete-stack --stack-name DataEngineeringStage-data-engineering-s3
 # aws cloudformation delete-stack --stack-name CDKToolkit
-
-
--- data_engineering/cdk_pipeline/cdk_data_engineering_pipeline.py
+++ data_engineering/cdk_pipeline/cdk_data_engineering_pipeline.py
@@ -7,29 +7,30 @@ from aws_cdk import (
 from constructs import Construct
 from .data_engineering_stage import DataEngineeringStage
 
-class CDKPipelineStack(Stack):
+
+class CDKDEPipelineStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs):
         super().__init__(scope, construct_id, **kwargs)
-        
+
         # Environment'ı sakla
-        self._env = kwargs.get('env')
-        
+        self._env = kwargs.get("env")
+
         # Context'ten parametreleri al
         project_name = self.node.try_get_context("project_name") or "data-engineering"
         notification_email = self.node.try_get_context("notification_email")
-        
+
         # GitHub connections information
         github_repo = "kanitvural/aws-data-science-data-engineering-mlops-infra"
         github_branch = "dataengineering"
         connection_arn = self.node.try_get_context("githubConnectionArn")
-        
+
         # Source aşaması
         source = pipelines_.CodePipelineSource.connection(
             repo_string=github_repo,
             branch=github_branch,
             connection_arn=connection_arn,
         )
-        
+
         synth_step = pipelines_.ShellStep(
             "Synth",
             input=source,
@@ -37,23 +38,23 @@ class CDKPipelineStack(Stack):
                 "npm install -g aws-cdk",
                 "pip install -r requirements.txt",
                 "cdk synth",
-            ]
+            ],
         )
-        
+
         # Pipeline'ı oluştur
         pipeline = pipelines_.CodePipeline(
             self,
             "CDKPipeline",
             synth=synth_step,
         )
-        
+
         # DataEngineeringStage'i parametrelerle oluştur
         data_eng_stage = DataEngineeringStage(
-            self, 
+            self,
             "DataEngineeringStage",
             env=self._env,
             project_name=project_name,
-            notification_email=notification_email
+            notification_email=notification_email,
         )
-        
+
         pipeline.add_stage(data_eng_stage)
