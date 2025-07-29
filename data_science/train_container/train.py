fabb03bfd5abdb4af4d99059487f7e69dad623ce
kanitvural <kanitvural@gmail.com>
2025-07-29 16:09:54 +0300

    train.py modified to implement combined and test csv files

--- data_science/train_container/train.py
+++ data_science/train_container/train.py
@@ -9,6 +9,8 @@ import logging
 
 TRAIN_CHANNEL = os.environ.get('SM_CHANNEL_TRAIN', '/opt/ml/input/data/train')
 VALIDATION_CHANNEL= os.environ.get('SM_CHANNEL_VALIDATION', '/opt/ml/input/data/validation')
+COMBINED_CHANNEL = os.environ.get('SM_CHANNEL_COMBINED', '/opt/ml/input/data/combined')
+TEST_CHANNEL = os.environ.get('SM_CHANNEL_TEST', '/opt/ml/input/data/test')
 MODEL_DIR = os.environ.get('SM_MODEL_DIR', '/opt/ml/model')
 FAILURE_DIR = os.environ.get('SM_FAILURE_DIR', '/opt/ml/failure')
 CONFIG_DIR = os.environ.get('SM_CONFIG_DIR', '/opt/ml/input/config')
@@ -17,6 +19,8 @@ OUTPUT_DIR = os.environ.get('SM_OUTPUT_DATA_DIR', '/opt/ml/output')
 HYPERPARAMS_PATH = os.path.join(CONFIG_DIR, 'hyperparameters.json')
 TRAIN_PATH = os.path.join(TRAIN_CHANNEL, 'train.csv')
 VALIDATION_PATH = os.path.join(VALIDATION_CHANNEL, 'validation.csv')
+COMBINED_PATH = os.path.join(COMBINED_CHANNEL, 'combined.csv')
+TEST_PATH = os.path.join(TEST_CHANNEL, 'test.csv')
 MODEL_PATH = os.path.join(MODEL_DIR, 'xgboost-model.json')
 FAILURE_PATH = os.path.join(FAILURE_DIR, 'failure')
 
@@ -61,6 +65,8 @@ def parse_args():
 
     parser.add_argument("--train-path", type=str, default=TRAIN_PATH)
     parser.add_argument("--validation-path", type=str, default=VALIDATION_PATH)
+    parser.add_argument("--combined-path", type=str, default=COMBINED_PATH)
+    parser.add_argument("--test-path", type=str, default=TEST_PATH)
     parser.add_argument("--model-path", type=str, default=MODEL_PATH)
     parser.add_argument("--hyperparameters-path", type=str, default=HYPERPARAMS_PATH)
     parser.add_argument("--failure-path", type=str, default=FAILURE_PATH)
@@ -80,20 +86,20 @@ def train(args):
         
         # Load train and validation datasets
         
-        if not os.path.exists(args.train_path):
-            logging.error(f"Training data file not found at: {args.train_path}")
-            sys.exit(1)
+        if args.combined_path and os.path.exists(args.combined_path):
+            logging.info(f"Loading combined data from {args.combined_path}")
+            train_df = pd.read_csv(args.combined_path)
+        else:
+            logging.info(f"Loading training data from {args.train_path}")
+            train_df = pd.read_csv(args.train_path)
+            
         
-        if not os.path.exists(args.validation_path):
-            logging.error(f"Validation data file not found at: {args.validation_path}")
-            sys.exit(1)
-        
-        logging.info(f"Loading training data from {args.train_path}")
-        train_df = pd.read_csv(args.train_path)
-        
-        logging.info(f"Loading validation data from {args.validation_path}")
-        val_df = pd.read_csv(args.validation_path)
-        
+        if args.test_path and os.path.exists(args.test_path):
+            logging.info(f"Loading test data from {args.test_path}")
+            val_df = pd.read_csv(args.test_path)
+        else:
+            logging.info(f"Loading validation data from {args.validation_path}")
+            val_df = pd.read_csv(args.validation_path)
 
         # Separate features and labels
         target ="dep_delay"
@@ -138,5 +144,6 @@ def train(args):
     
 
 if __name__ == "__main__":
+    logging.info(f"sys.argv: {sys.argv}")
     args, _ = parse_args()
     train(args)
