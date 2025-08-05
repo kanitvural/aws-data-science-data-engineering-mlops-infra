b8bc157c0db4c51484d693c806928bee059e6f8e
kanitvural <kanitvural@gmail.com>
2025-08-05 09:25:24 +0300

    pip install --upgrade pip addec to commands on cdk main pipeline

--- data_science/cdk_pipeline/cdk_data_science_pipeline.py
+++ data_science/cdk_pipeline/cdk_data_science_pipeline.py
@@ -44,7 +44,7 @@ class CDKDataSciencePipelineStack(Stack):
         training_instance_type = "ml.t3.large"
         clarify_instance_count = 1
         clarify_instance_type = "ml.t3.large"
-        rmse_threshold = 15.0
+        rmse_threshold = 5.0
         max_jobs = 1
         max_parallel_jobs = 1
 
@@ -59,6 +59,7 @@ class CDKDataSciencePipelineStack(Stack):
             input=source,
             commands=[
                 "npm install -g aws-cdk",
+                "pip install --upgrade pip",
                 "pip install -r requirements.txt",
                 "cdk synth --context @aws-cdk/core:bootstrapQualifier=ds",
             ],
