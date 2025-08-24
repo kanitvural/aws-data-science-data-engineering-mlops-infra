9cc9e3d3678d9ceb446a9154c74b033f49f93103
kanitvural <kanitvural@gmail.com>
2025-08-24 14:06:47 +0300

    rmse_threshold = 11.0

--- data_science/cdk_pipeline/cdk_data_science_pipeline.py
+++ data_science/cdk_pipeline/cdk_data_science_pipeline.py
@@ -44,7 +44,7 @@ class CDKDataSciencePipelineStack(Stack):
         training_instance_type = "ml.t3.large"
         clarify_instance_count = 1
         clarify_instance_type = "ml.t3.large"
-        rmse_threshold = 9.0
+        rmse_threshold = 11.0
         max_jobs = 10
         max_parallel_jobs = 2
 
