a41055d0153a244b761e770c98dfd5987a756116
kanitvural <kanitvural@gmail.com>
2025-09-15 22:07:59 +0300

    eventbridge permissions added to the sns

--- data_engineering/stacks/sns_stack.py
+++ data_engineering/stacks/sns_stack.py
@@ -2,14 +2,14 @@ from aws_cdk import (
     Stack,
     aws_sns as sns,
     aws_sns_subscriptions as subs,
+    aws_iam as iam,
     CfnOutput
- 
 )
 from constructs import Construct
 
 
 class SNSStack(Stack):
-    def __init__(self, scope: Construct, id: str,project_name: str, notification_email: str, **kwargs):
+    def __init__(self, scope: Construct, id: str, project_name: str, notification_email: str, **kwargs):
         super().__init__(scope, id, **kwargs)
 
         self.topic = sns.Topic(
@@ -19,9 +19,30 @@ class SNSStack(Stack):
 
         self.topic.add_subscription(subs.EmailSubscription(notification_email))
         
+        self.topic.add_to_resource_policy(
+            iam.PolicyStatement(
+                sid="AllowEventBridgeToPublish",
+                effect=iam.Effect.ALLOW,
+                principals=[iam.ServicePrincipal("events.amazonaws.com")],
+                actions=["sns:Publish"],
+                resources=[self.topic.topic_arn],
+                conditions={
+                    "StringEquals": {
+                        "aws:SourceAccount": self.account
+                    }
+                }
+            )
+        )
+        
         CfnOutput(
             self, "SNSNotificationTopicArn",
             value=self.topic.topic_arn,
             export_name=f"{project_name}-sns-topic-arn"
         )
 
+        # Debug için topic policy'yi de export et
+        CfnOutput(
+            self, "SNSTopicPolicy",
+            value=self.topic.topic_name,
+            description="SNS Topic Name for debugging"
+        )
