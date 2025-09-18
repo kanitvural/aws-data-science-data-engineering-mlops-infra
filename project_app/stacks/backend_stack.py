871f3c5f5f02d50f278348d75774ee503debef8e
kanitvural <kanitvural@gmail.com>
2025-09-18 16:09:05 +0300

    API Gateway CORS added

--- project_app/stacks/backend_stack.py
+++ project_app/stacks/backend_stack.py
@@ -1,7 +1,6 @@
 from aws_cdk import Stack, aws_lambda as _lambda, aws_apigateway as apigw, aws_iam as iam, CfnOutput, Fn
 from constructs import Construct
 
-
 class BackendStack(Stack):
     def __init__(self, scope: Construct, id: str, project_name: str, **kwargs):
         super().__init__(scope, id, **kwargs)
@@ -30,14 +29,27 @@ class BackendStack(Stack):
             )
         )
 
-        api = apigw.LambdaRestApi(
+        # API Gateway
+        api = apigw.RestApi(
             self,
             "FlightsApi",
-            handler=flight_lambda,
-            proxy=True,
             rest_api_name="Flights API",
+            default_cors_preflight_options=apigw.CorsOptions(
+                allow_origins=apigw.Cors.ALL_ORIGINS,
+                allow_methods=["GET", "OPTIONS"],
+            ),
         )
 
+        # /flights resource
+        flights_resource = api.root.add_resource("flights")
+
+        flights_resource.add_method(
+            "GET",
+            apigw.LambdaIntegration(flight_lambda),
+        )
+
+
+        # API URL output
         CfnOutput(
             self,
             "FlightsApiUrl",
