e734412b828a19a8fb1234c13747563dc40e1fbf
kanitvural <kanitvural@gmail.com>
2025-09-24 08:21:58 +0300

    rest api removed from pipeline

--- project_app/cdk_pipeline/project_app_stage.py
+++ project_app/cdk_pipeline/project_app_stage.py
@@ -6,7 +6,7 @@ from project_app.stacks.ec2_stack import EC2Stack
 from project_app.stacks.lambda_stack import LambdaStack
 from project_app.stacks.sns_stack import SNSStack
 from project_app.stacks.dynamodb_stack import DynamoDBStack
-from project_app.stacks.api_gateway_rest_stack import ApiGatewayRestStack
+# from project_app.stacks.api_gateway_rest_stack import ApiGatewayRestStack
 from project_app.stacks.api_gateway_websocket_stack import ApiGatewayWebSocketStack
 
 
@@ -51,11 +51,11 @@ class AppPipelineStage(Stage):
             project_name=project_name
         )
         
-        api_gateway_rest_stack = ApiGatewayRestStack(
-            self,
-            id="ApiGatewayRestInfrastructure",
-            project_name=project_name
-        )
+        # api_gateway_rest_stack = ApiGatewayRestStack(
+        #     self,
+        #     id="ApiGatewayRestInfrastructure",
+        #     project_name=project_name
+        # )
         
         api_gateway_websocket_stack = ApiGatewayWebSocketStack(
             self,
@@ -64,10 +64,10 @@ class AppPipelineStage(Stage):
         )
 
         # Dependencies 
+      
         dynamodb_stack.add_dependency(sns_stack)
-        api_gateway_rest_stack.add_dependency(dynamodb_stack)  
-        api_gateway_websocket_stack.add_dependency(sns_stack)  
-        dynamodb_stack.add_dependency(api_gateway_websocket_stack) 
+        api_gateway_websocket_stack.add_dependency(sns_stack)
+        dynamodb_stack.add_dependency(api_gateway_websocket_stack)  
         kinesis_stack.add_dependency(dynamodb_stack)
         lambda_stack.add_dependency(kinesis_stack)
         s3_stack.add_dependency(lambda_stack)
