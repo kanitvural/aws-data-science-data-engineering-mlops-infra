c4642c98d4af2912e38bf186c8b179cf8a87d59e
kanitvural <kanitvural@gmail.com>
2025-10-02 16:33:00 +0300

    fetch func implemented to the fronthand for getting agent memory back

---
+++ project_app/frontend/_next/static/IGYvuF_IGq9hZRE6m7ktk/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/e10792fe7f525fb6.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ project_app/frontend/server/app/favicon.ico/route.js
@@ -0,0 +1,6 @@
+var R=require("../../chunks/[turbopack]_runtime.js")("server/app/favicon.ico/route.js")
+R.c("server/chunks/c485d_next_7979991b._.js")
+R.c("server/chunks/[root-of-the-server]__29ea6097._.js")
+R.m("[project]/flight-dashboard/.next-internal/server/app/favicon.ico/route/actions.js [app-rsc] (server actions loader, ecmascript)")
+R.m("[project]/flight-dashboard/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/flight-dashboard/src/app/favicon--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)")
+module.exports=R.m("[project]/flight-dashboard/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/flight-dashboard/src/app/favicon--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)").exports
