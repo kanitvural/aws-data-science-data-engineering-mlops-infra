2dd28e43edb2d5df2fb72e96bccb6c1531614154
kanitvural <kanitvural@gmail.com>
2025-10-19 08:00:22 +0300

    OAC added

--- project_app/stacks/s3_stack.py
+++ project_app/stacks/s3_stack.py
@@ -3,6 +3,7 @@ from aws_cdk import (
     aws_s3 as s3,
     aws_cloudfront as cloudfront,
     aws_cloudfront_origins as origins,
+    aws_iam as iam,
     RemovalPolicy,
     CfnOutput,
     Duration
@@ -12,7 +13,7 @@ from constructs import Construct
 class S3Stack(Stack):
     def __init__(self, scope: Construct, id: str, project_name: str, **kwargs):
         super().__init__(scope, id, **kwargs)
-
+        
         # ----------------------------------------------------------------------
         # Private S3 Bucket
         # ----------------------------------------------------------------------
@@ -23,32 +24,36 @@ class S3Stack(Stack):
             versioned=True,
             removal_policy=RemovalPolicy.DESTROY,
             auto_delete_objects=True,
-            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,  # private
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
         )
-
+        
+        # ----------------------------------------------------------------------
+        # Origin Access Control (OAC) - AWS recommended modern approach
+        # ----------------------------------------------------------------------
+        oac = cloudfront.CfnOriginAccessControl(
+            self,
+            "OAC",
+            origin_access_control_config=cloudfront.CfnOriginAccessControl.OriginAccessControlConfigProperty(
+                name=f"{project_name}-OAC",
+                origin_access_control_origin_type="s3",
+                signing_behavior="always",
+                signing_protocol="sigv4"
+            )
+        )
+        
         # ----------------------------------------------------------------------
         # CloudFront Distribution
         # ----------------------------------------------------------------------
-        oai = cloudfront.OriginAccessIdentity(
-            self,
-            "CloudFrontOAI",
-            comment="OAI for private S3 access"
-        )
-
-        # Bucket'a CloudFront erişimi ver
-        bucket.grant_read(oai)
-
         distribution = cloudfront.Distribution(
             self,
             "ProjectAppDistribution",
             default_behavior=cloudfront.BehaviorOptions(
-                origin=origins.S3BucketOrigin(bucket, origin_access_identity=oai),
+                origin=origins.S3BucketOrigin(bucket),
                 viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.HTTPS_ONLY,
-                allowed_methods=cloudfront.AllowedMethods.ALLOW_GET_HEAD,  # for static files
+                allowed_methods=cloudfront.AllowedMethods.ALLOW_GET_HEAD,
                 cache_policy=cloudfront.CachePolicy.CACHING_OPTIMIZED,
             ),
-            default_root_object="index.html",  # CloudFront root object
-            
+            default_root_object="index.html",
             error_responses=[
                 cloudfront.ErrorResponse(
                     http_status=403,
@@ -63,9 +68,34 @@ class S3Stack(Stack):
                     ttl=Duration.seconds(0),
                 ),
             ],
-            price_class=cloudfront.PriceClass.PRICE_CLASS_100  # NA & EU
+            price_class=cloudfront.PriceClass.PRICE_CLASS_100
         )
-
+        
+        # ----------------------------------------------------------------------
+        # CRITICAL: Attach OAC to CloudFront Distribution (L1 override)
+        # ----------------------------------------------------------------------
+        cfn_distribution = distribution.node.default_child
+        cfn_distribution.add_property_override(
+            "DistributionConfig.Origins.0.OriginAccessControlId",
+            oac.attr_id
+        )
+        
+        # ----------------------------------------------------------------------
+        # Bucket Policy - Grant CloudFront access via OAC
+        # ----------------------------------------------------------------------
+        bucket.add_to_resource_policy(
+            iam.PolicyStatement(
+                actions=["s3:GetObject"],
+                resources=[f"{bucket.bucket_arn}/*"],
+                principals=[iam.ServicePrincipal("cloudfront.amazonaws.com")],
+                conditions={
+                    "StringEquals": {
+                        "AWS:SourceArn": f"arn:aws:cloudfront::{self.account}:distribution/{distribution.distribution_id}"
+                    }
+                }
+            )
+        )
+        
         # ----------------------------------------------------------------------
         # Outputs
         # ----------------------------------------------------------------------
@@ -76,7 +106,7 @@ class S3Stack(Stack):
             description="S3 Bucket name for ProjectApp Frontend",
             export_name="ProjectAppBucketName",
         )
-
+        
         CfnOutput(
             self,
             "CloudFrontURL",
